e5ce6b74bb9a91cbc1d9fedd144c315a37be0815
sirben181 <bensonkamauben@gmail.com>
2021-12-23 23:01:51 +0300

    adding some media queries

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,31 @@
 import React from 'react'
-
+import './home.css'
+import home from '.././components/images/home.jpg'
+import { Link } from 'react-router-dom'
 const Home = () => {
     return (
-        <div>
-            Home
-            Hom
-            Home
-            Home
-            Home
-            Home
+        <div className='home_container'>
+            <div className='landing_page'>
+               <img src={home}  alt={home}/>
+            </div>
+            <div className="landing_message">
+                <h2> Welcome, we have a alot of services all tech </h2>
+                <Link to='/services'>
+                  <button className="btn_primary">
+                     Learn more
+                   </button>
+                </Link>
+            </div>
+            <div className="">
+                <h2>Creating a wordpress themes</h2>
+                <p> Join us and we are going to create a wordpress them for you,depending on your choices.
+                    if you dont hacve those choices we can help you decide and make  your dream come true.
+                    We are the best wordpress developers around.
+
+                </p>
+                <p>Here are some of the projects we have worked on</p>
+            </div>
+           
         </div>
     )
 }
