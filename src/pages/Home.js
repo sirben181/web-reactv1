b544e965a6daeb4d98ac057a38c0cbf28346a3b3
sirben181 <bensonkamauben@gmail.com>
2021-12-23 23:17:30 +0300

    home content

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,14 +16,18 @@ const Home = () => {
                    </button>
                 </Link>
             </div>
-            <div className="">
+            <div className="wordpress_excerpt_home">
                 <h2>Creating a wordpress themes</h2>
                 <p> Join us and we are going to create a wordpress them for you,depending on your choices.
                     if you dont hacve those choices we can help you decide and make  your dream come true.
                     We are the best wordpress developers around.
 
                 </p>
-                <p>Here are some of the projects we have worked on</p>
+                <p>Here are some of the projects we have worked on
+                    <Link to=''>
+                        <button className='btn_primary'>Read more</button>
+                    </Link>
+                </p>
             </div>
            
         </div>
