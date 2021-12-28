90d37cdda4c8cb8488a38adf9dcd025262b46ef3
sirben181 <bensonkamauben@gmail.com>
2021-12-28 10:11:15 +0300

    more page content

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,17 +17,74 @@ const Home = () => {
                 </Link>
             </div>
             <div className="wordpress_excerpt_home">
-                <h2>Creating a wordpress themes</h2>
-                <p> Join us and we are going to create a wordpress them for you,depending on your choices.
-                    if you dont hacve those choices we can help you decide and make  your dream come true.
+                <div className='wordpress_excerpt_wrapper'>
+                    <h2>Creating a wordpress themes</h2>
+                    <p> Join us and we are going to create a wordpress them for you,depending on your choices.
+                    if you dont have those choices we can help you decide and make  your dream come true.
                     We are the best wordpress developers around.
 
-                </p>
-                <p>Here are some of the projects we have worked on
-                    <Link to=''>
-                        <button className='btn_primary'>Read more</button>
-                    </Link>
-                </p>
+                    </p>
+                    <p>Here are some of the projects we have worked on
+                      
+                    </p>
+                    <Link to='/services'>
+                            <button className='btn_primary'>Read more</button>
+                   </Link>
+                </div>
+                <div className='wordpress_excerpt_wrapper'>
+                    <h2>Landing pages</h2>
+                    <p> Join us and we are going to create a wordpress them for you,depending on your choices.
+                    if you dont have those choices we can help you decide and make  your dream come true.
+                    We are the best wordpress developers around.
+
+                    </p>
+                    <p>Here are some of the projects we have worked on
+                      
+                    </p>
+                    <Link to='/services'>
+                            <button className='btn_primary'>Read more</button>
+                   </Link>
+                </div><div className='wordpress_excerpt_wrapper'>
+                    <h2>webmaintenance</h2>
+                    <p> Join us and we are going to create a wordpress them for you,depending on your choices.
+                    if you dont have those choices we can help you decide and make  your dream come true.
+                    We are the best wordpress developers around.
+
+                    </p>
+                    <p>Here are some of the projects we have worked on
+                      
+                    </p>
+                    <Link to='/services'>
+                            <button className='btn_primary'>Read more</button>
+                   </Link>
+                </div>
+                <div className='wordpress_excerpt_wrapper'>
+                    <h2>Webdesign</h2>
+                    <p> Join us and we are going to create a wordpress them for you,depending on your choices.
+                    if you dont have those choices we can help you decide and make  your dream come true.
+                    We are the best wordpress developers around.
+
+                    </p>
+                    <p>Here are some of the projects we have worked on
+                      
+                    </p>
+                    <Link to='/services'>
+                            <button className='btn_primary'>Read more</button>
+                   </Link>
+                </div><div className='wordpress_excerpt_wrapper'>
+                    <h2>Domain registration</h2>
+                    <p> Join us and we are going to create a wordpress them for you,depending on your choices.
+                    if you dont have those choices we can help you decide and make  your dream come true.
+                    We are the best wordpress developers around.
+
+                    </p>
+                    <p>Here are some of the projects we have worked on
+                      
+                    </p>
+                    <Link to='/services'>
+                            <button className='btn_primary'>Read more</button>
+                   </Link>
+                </div>
             </div>
            
         </div>
--- src/pages/Service.js
+++ src/pages/Service.js
@@ -2,20 +2,49 @@ import React from 'react'
 
 const Service = () => {
     return (
-        <div>
-        <h2> heloo there how are toi doing over there</h2>
-        <h2> heloo there how are toi doing over there</h2>
-        <h2> heloo there how are toi doing over there</h2>
-        <h2> heloo there how are toi doing over there</h2>
-        <h2> heloo there how are toi doing over there</h2>
-        <h2> heloo there how are toi doing over there</h2>
-        <h2> heloo there how are toi doing over there</h2>
-        <h2> heloo there how are toi doing over there</h2>
-        <h2> heloo there how are toi doing over there</h2>
-        <h2> heloo there how are toi doing over there</h2>
-        <h2> heloo there how are toi doing over there</h2>
-        <h2> heloo there how are toi doing over there</h2>
-        </div>
+        <div className="service_conatiner">
+            <div className='services_wrapper'>
+                <div className='services_items'>
+                    <div className='domain_service'>
+                        <h3>Domain registration</h3>
+                    <p> The best domain registration processing company.
+                        we will help you have a good domain and registered to the company of your choice.
+                        We will also help you to know the best domain services around
+                    </p>
+                    </div>
+                    <div className='wordpress'>
+                        <h3>Wordpress</h3>
+                    <p> The best domain registration processing company.
+                        we will help you have a good domain and registered to the company of your choice.
+                        We will also help you to know the best domain services around
+                    </p>
+                    </div>
+                    <div className='landingpage_service'>
+                        <h3>landingpages</h3>
+                    <p> The best domain registration processing company.
+                        we will help you have a good domain and registered to the company of your choice.
+                        We will also help you to know the best domain services around
+                    </p>
+                    </div>
+                    <div className='webdesign_service'>
+                        <h3>designing websites</h3>
+                    <p> The best domain registration processing company.
+                        we will help you have a good domain and registered to the company of your choice.
+                        We will also help you to know the best domain services around
+                    </p>
+                    </div>
+                    <div className='webmaintenance'>
+                        <h3>Webmaintenance</h3>
+                    <p> The best domain registration processing company.
+                        we will help you have a good domain and registered to the company of your choice.
+                        We will also help you to know the best domain services around
+                    </p>
+                    </div>
+                </div>
+            </div>
+
+      </div>
+      
     )
 }
 
