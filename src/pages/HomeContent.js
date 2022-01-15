11e6bafe3a3655d895c48d8d480724af7b13fcf5
sirben181 <bensonkamauben@gmail.com>
2022-01-15 23:03:55 +0300

    more developed

--- src/pages/HomeContent.js
+++ src/pages/HomeContent.js
@@ -2,7 +2,11 @@
 import React from 'react'
 
 export const HomeContent = [
-    { className:'excerpt_wrapper',
+    { 'className ':'excerpt_wrapper',
+      'h2':'webdesign',
+      'paragraph': "Join us and we are going to create a wordpress them for you,depending on your choices",
+           
+    
      
 
 }]
