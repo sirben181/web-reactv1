8db0a41c0c7faf3fef966e87230a347687a44427
sirben181 <bensonkamauben@gmail.com>
2022-01-18 07:53:17 +0300

    more pages design

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -4,21 +4,27 @@ import './blog.css'
 const Blog = () => {
     return (
         <div className="blog_container">
+            <div className="blog_wrapper">
              <h1>Hell here is the blog</h1>
              <p>I wanted to be a teacher on YouTube teaching how to code, and I got the screen play recorder obs and I have net and started doing it but the machine am using can’t handle all this at the same time. So I have to come up with a different option and different things to do. At least for now it seems impossible because I need major machine upgrade.
 What’s the way forward then? The way forward has to be done just right now. I have to decide it right now. What is the first and my main priority? My only priority is that I want to earn and I want to make a living as a software developer. Jobs seems like it will take hell long simply because most seems like they are taking people with at least experience of at least 3yrs.
 You need those three yrs. to get a job and then you need a job to get that kind of experience. That’s kind of sick. So what should I do then to make a living in this career sooner, in short what can I do to just pay my bills with it. It would be an absolute honor to be able to do this.
 What are my options:
+</p>
+<p>
 •	 Freelancing- this is not a cool thing unless done differently because it’s a bit hard to get started in this. It’s like climbing MT Everest to get the first job. I don’t know what I have to do to make it work like seriously.
 Is it even worth it? Is it something you can actually depend on? And if yes how am I supposed to do that and make a living on it? To be able to support my family with this kind of job? I have absolute control over these kinds of jobs. This is can only be a side hustle and not a fully dependent job.
 •	The other option I think is to make a blog and start blogging and see how it goes. Some of the blogs that I can’t write is health, sports and tech blog posts. What kind of language or programming stack that I will have to use; is it react, firebase or mern stack, or even vanilla js with MySQL db, or is it a static kind of websites?
-•	What other options do I have? Any other? Am actually thinking that the only option for now is if I make a blog and let myself off the grid. I will have something to show for all the six months. I will show and say that this is what I can do and am unstoppable and I can do everything I say I will. This is a good step to show what I can do and show what I can’t do even if I say am going to do it. So this is to prove that either am a procrastinator or somebody who can actually make decisions and follow them.
-This is the best thing that can happen to me. Why? This is because it can help me to actually show people what am made of. I want to show them that am made of a procrastinating douchebag or a decisive kind of guy. It’s like I have fallen for the former and that’s if I get back to the job I was actually fearing to continue doing with nothing to show then I believe this is the worst part and version of me. If I continue doing this am going to end up trapped in what I was actually fearing and I will look like just I wasted my 6 months and the bike time and money, so this has to be taken into account and done very well.
+•	What other options do I have? Any other</p>
+<p>Am actually thinking that the only option for now is if I make a blog and let myself off the grid. I will have something to show for all the six months. I will show and say that this is what I can do and am unstoppable and I can do everything I say I will. This is a good step to show what I can do and show what I can’t do even if I say am going to do it. So this is to prove that either am a procrastinator or somebody who can actually make decisions and follow them.
+This is the best thing that can happen to me. Why? This is because it can help me to actually show people what am made of.</p>
+<p> I want to show them that am made of a procrastinating douchebag or a decisive kind of guy. It’s like I have fallen for the former and that’s if I get back to the job I was actually fearing to continue doing with nothing to show then I believe this is the worst part and version of me. If I continue doing this am going to end up trapped in what I was actually fearing and I will look like just I wasted my 6 months and the bike time and money, so this has to be taken into account and done very well.
 I like react, firebase and likes of mongo db and I think am going to create a react blog unlike using the vanilla js. I am going to decide this now and see what am actually going to do. Here is after I have made this then what next?
 I just get and grab any job and start to write down the best stories…and then grab other jobs for survival at least I can show at least I got something to show if given a chance I would have done something interesting.
  What can I write about and be happy that am writing.
 
-</p>
+</p> 
+           </div>
         </div>
     )
 }
