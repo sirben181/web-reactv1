907096bc599daafae60d7df8b5fb32e27179f4d7
sirben181 <bensonkamauben@gmail.com>
2022-02-28 23:12:20 +0300

    more

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import './about.css'
+// import Avatar from ''
 const About = () => {
     return (
         <div className="about_container">
             <div className="about_wrapper">
                 <h2>About</h2>
-                <p> our statement:<scan>To be the best programmeer around</scan> </p>
-                <p> Our company story: follow us on <scan>blog kabisa</scan></p>
-                <p> who we serving and offering : we are offering some good news</p>
-                <p> who served :wer have served some of the best brands </p>
-                <p> values : humilty, diligence and communication,  </p>
+                <p> our statement:<scan>To be the best software developement platform in kenya and also offer software courses and teaching courses</scan> </p>
+                {/*  the initial idea is just providing software services and designing websites
+                websites maintenance
+                wordpress services
+                eccommerce services
+                blog and all other types of websites */}
             </div>
         </div>
     )
