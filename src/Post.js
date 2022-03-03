4468e89d55407684940fb8287a43e2ff043bb7b4
sirben181 <bensonkamauben@gmail.com>
2022-03-03 22:29:56 +0300

    posts looped and limited

--- src/Post.js
+++ src/Post.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
-const Post = () => {
+const Post = ({posts}) => {
+    // if(loading){
+    //     return <h2>Loading...</h2>
+    // }
   return (
-        <div className='control-container'>
-            
+        <div className='post_container'>
+            {posts.map(post=>(
+                <div className='post'
+                key={post.id}>
+                  <h2>{post.title}</h2>
+                  <p>{post.body}</p>
+                </div>
+            ))}
         </div>
      )
 }
--- src/pages/Service.js
+++ src/pages/Service.js
@@ -1,5 +1,6 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import  './service.css'
+import Post from '../Post';
 const Service = () => {
     const[posts,setPosts]=useState([
         {id:1,
@@ -8,21 +9,63 @@ const Service = () => {
         },
         {id:2,
         title:"welcome here this is the best thing you can do over here",
-         body:"bienvidos amigos"},
+         body:"bienvidos amigos"
+        },
          {
              id:3,
              title:"where are u doing these things over here",
              body:"this is the thing we are supposed to do the things "
-         }
+         },  {
+            id:4,
+            title:"where are u doing these things over here",
+            body:"this is the thing we are supposed to do the things "
+        },
+        {
+            id:5,
+            title:"where are u doing these things over here",
+            body:"this is the thing we are supposed to do the things "
+        },  {
+            id:6,
+            title:"where are u doing these things over here",
+            body:"this is the thing we are supposed to do the things "
+        },  {
+            id:7,
+            title:"where are u doing these things over here",
+            body:"this is the thing we are supposed to do the things "
+        },  {
+            id:8,
+            title:"where are u doing these things over here",
+            body:"this is the thing we are supposed to do the things "
+        },  {
+            id:9,
+            title:"where are u doing these things over here",
+            body:"this is the thing we are supposed to do the things "
+        },  {
+            id:10,
+            title:"where are u doing these things over here",
+            body:"this is the thing we are supposed to do the things "
+        },  {
+            id:11,
+            title:"where are u doing these things over here",
+            body:"this is the thing we are supposed to do the things "
+        },  {
+            id:12,
+            title:"where are u doing these things over here",
+            body:"this is the thing we are supposed to do the things "
+        }
     ])
-    const [loading,setLoading]=useState(false)
+    // const [loading,setLoading]=useState(false)
     const [currentPage,setCurrentPage]=useState(1)
+    const [postsPerPage,setPostsPerPage]=useState(5)
     
-   
+   const indexOfLastPost=currentPage*postsPerPage;
+   const indexOfFirstPost=indexOfLastPost-postsPerPage
+   const currentPosts=posts.slice(indexOfFirstPost,indexOfLastPost)
     return (
         <div className="service_container">
             <div className='services_wrapper'>
-                
+               <Post posts={currentPosts}
+               />
             </div>
 
       </div>
